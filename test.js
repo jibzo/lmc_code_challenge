5603725576dce3cc8209b02d697c5b8156333c81
Adam Hurst <adam@logmycare.co.uk>
2018-11-15 11:52:12 +0000

    fix broken tests

--- test.js
+++ test.js
@@ -8,29 +8,30 @@ const {
 
 describe('fibonacci method', () => {
 	it('should return Fibonacci Sequence', () => {
-		assert.equal(fibonacci(0), 1);
+		assert.equal(fibonacci(0), 0);
 		assert.equal(fibonacci(1), 1);
-		assert.equal(fibonacci(2), 2);
-		assert.equal(fibonacci(3), 3);
-		assert.equal(fibonacci(4), 5);
-		assert.equal(fibonacci(5), 8);
-		assert.equal(fibonacci(6), 13);
+		assert.equal(fibonacci(2), 1);
+		assert.equal(fibonacci(3), 2);
+		assert.equal(fibonacci(4), 3);
+		assert.equal(fibonacci(5), 5);
+		assert.equal(fibonacci(6), 8);
+		assert.equal(fibonacci(7), 13);
 	});
 });
 
 describe('flatten method', () => {
 	it('should flatten array', () => {
-		assert.equal(flatten([1, 2]), [1, 2]);
-		assert.equal(flatten([1, [2]]), [1, 2]);
-		assert.equal(flatten([1, [2, [3]]]), [1, 2, 3]);
+		assert.deepEqual(flatten([1, 2]), [1, 2]);
+		assert.deepEqual(flatten([1, [2]]), [1, 2]);
+		assert.deepEqual(flatten([1, [2, [3]]]), [1, 2, 3]);
 	});
 });
 
 describe('countLetters method', () => {
 	it('should count the number of letters in a string', () => {
-		assert.equal(countLetters('AAA'), { A: 3 });
-		assert.equal(countLetters('AAC'), { A: 3, C: 1 });
-		assert.equal(countLetters('AAACCTG'), { A: 3, C: 2, G: 1, T: 1 });
+		assert.deepEqual(countLetters('AAA'), { A: 3 });
+		assert.deepEqual(countLetters('AAC'), { A: 2, C: 1 });
+		assert.deepEqual(countLetters('AAACCTG'), { A: 3, C: 2, G: 1, T: 1 });
 	});
 });
 
